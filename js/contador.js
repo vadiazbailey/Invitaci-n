3f153842e99c7e6290311c539efba8c03a94b995
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-12-16 16:29:47 -0300

    agregadas ubicaciones

---
+++ js/contador.js
@@ -0,0 +1,28 @@
+"use strict"
+
+var countDownDate = new Date(1645196400000).getTime();
+
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.querySelector(".contador").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".contador").innerHTML = "EXPIRED";
+  }
+}, 1000);
