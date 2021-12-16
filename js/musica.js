7b620b67e83e61ddce83cf34b19be5d60bbef65c
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-12-15 21:31:40 -0300

    Musica agregada al boton

---
+++ js/musica.js
@@ -0,0 +1,13 @@
+var myAudio = document.getElementById("cancion");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
